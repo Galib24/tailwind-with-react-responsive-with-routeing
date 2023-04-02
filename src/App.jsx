6a9d79308ffd74aba1e250070d4a8a91ec355733
SQLAdmin <abuyeahia24@gmail.com>
2023-04-03 03:48:32 +0600

    added some chart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import PriceList from './components/price List/PriceList'
 import DashBoard from './components/Dash Borad/DashBoard'
+import PhoneBar from './components/PhoneBar/PhoneBar'
 
 
 function App() {
@@ -14,8 +15,7 @@ function App() {
       <h1 className='text-7xl mb-5 text-center text-purple-500'>hello pal</h1>
       <PriceList></PriceList>
       <DashBoard></DashBoard>
-
-
+      <PhoneBar></PhoneBar>
     </div>
   )
 }
